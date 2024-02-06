27486557ce39457bd83cd1d6f4d8744d07b892b7
Andrés Ibáñez Ulloa <andresibanezulloa@gmail.com>
2024-02-05 23:05:36 -0300

    LineItem add_product

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -1,5 +1,7 @@
 class LineItemsController < ApplicationController
+  include CurrentCart
   before_action :set_line_item, only: %i[ show edit update destroy ]
+  before_action :set_cart, only: :create
 
   # GET /line_items or /line_items.json
   def index
@@ -21,8 +23,9 @@ class LineItemsController < ApplicationController
 
   # POST /line_items or /line_items.json
   def create
-    @line_item = LineItem.new(line_item_params)
-
+    product = Product.find(params[:product_id])
+    @line_item = LineItem.add_product(product)
+    @line_item.cart = @cart
     respond_to do |format|
       if @line_item.save
         format.html { redirect_to line_item_url(@line_item), notice: "Line item was successfully created." }
--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,14 @@ class LineItem < ApplicationRecord
   belongs_to :product
   belongs_to :cart, optional: true
   belongs_to :order, optional: true
+
+  def self.add_product(product)
+    line_product = LineItem.find_by(product_id: product.id)
+    if line_product
+      line_product.quantity += 1
+    else
+      line_product = new(product_id: product.id, price: product.price)
+    end
+    return line_product
+  end
 end
