dede05c5ce509fe6d5b541ae0a586fb580c20a2b
Andrés Ibáñez Ulloa <andresibanezulloa@gmail.com>
2024-02-05 21:21:46 -0300

    lambda products, view index store

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,5 @@
 class StoreController < ApplicationController
   def index
+    @products = Product.order_by_title
   end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,3 @@
 class Product < ApplicationRecord
+  scope :order_by_title, -> {order(title: :asc) }
 end
